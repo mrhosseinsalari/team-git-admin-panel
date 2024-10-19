87f1cdc0f87ae7a30aeb08c4b2ec4c008a3a5d80
Hossein Salari <hosein.salari81@gmail.com>
2024-10-19 20:35:09 +0330

    feat: maxwidth added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,12 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      maxWidth: {
+        containerWidth: "1920px",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
