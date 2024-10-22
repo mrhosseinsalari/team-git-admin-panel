b19dab4079e7dba84efc806c19a61503204556c7
Hossein Salari <hosein.salari81@gmail.com>
2024-10-22 20:01:22 +0330

    sidebar added

---
+++ utils/jsonChecker.ts
@@ -0,0 +1,19 @@
+export const isJson = (res?: Response, str?: string) => {
+  if (res) {
+    const result = res.headers
+      .get("content-type")
+      ?.includes("application/json");
+
+    return result;
+  } else if (str) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  return false;
+};
